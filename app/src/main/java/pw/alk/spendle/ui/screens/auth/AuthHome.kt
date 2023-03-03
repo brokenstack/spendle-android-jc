43fdbc3e1aeb3c663c4642a0311ebafcdacb0067
alok8bb <alok8bb@gmail.com>
2023-03-03 21:42:02 +0530

    feat: design auth home
    Signed-off-by: alok8bb <alok8bb@gmail.com>

--- app/src/main/java/pw/alk/spendle/ui/screens/auth/AuthHome.kt
+++ app/src/main/java/pw/alk/spendle/ui/screens/auth/AuthHome.kt
@@ -1,16 +1,110 @@
 package pw.alk.spendle.ui.screens.auth
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Email
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import pw.alk.spendle.R
+import pw.alk.spendle.ui.theme.White80
+
+val ButtonModifier = Modifier
+    .height(60.dp)
+    .fillMaxWidth()
 
 @Composable
-fun AuthHome(){
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center)
+fun AuthHome() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(vertical = 40.dp, horizontal = 15.dp),
+        verticalArrangement = Arrangement.Bottom,
+        horizontalAlignment = Alignment.CenterHorizontally
+    )
     {
-        Text("Auth Home")
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .padding(vertical = 30.dp),
+            verticalArrangement = Arrangement.Bottom,
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.lg_spendle),
+                contentDescription = "Spendle Logo",
+                modifier = Modifier.size(120.dp)
+            )
+            Text("Spendle", style = MaterialTheme.typography.h2)
+            Text(
+                "Track your expenses effortlessly",
+                style = MaterialTheme.typography.h6
+            )
+        }
+        Spacer(modifier = Modifier.height(70.dp))
+
+        Button(
+            onClick = { /*TODO*/ },
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = MaterialTheme.colors.primary
+            ),
+            modifier = ButtonModifier,
+        ) {
+            Icon(
+                Icons.Outlined.Email,
+                contentDescription = "Email",
+                tint = White80
+            )
+            Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
+            Text(
+                text = "Register with Email ID",
+                style = MaterialTheme.typography.button
+            )
+        }
+
+        Spacer(modifier = Modifier.height(13.dp))
+        OutlinedButton(onClick = { /*TODO*/ }, modifier = ButtonModifier) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_google),
+                modifier = Modifier.size(26.dp),
+                contentDescription = "Google Icon"
+            )
+            Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
+            Text(
+                "Sign Up with Google",
+                style = MaterialTheme.typography.button,
+            )
+        }
+
+        Spacer(modifier = Modifier.height(80.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text("Already have an account? ", fontSize = 17.sp)
+            ClickableText(text = buildAnnotatedString {
+                withStyle(
+                    style = SpanStyle(
+                        color = White80,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 17.sp,
+                        textDecoration = TextDecoration.Underline
+                    )
+                ) {
+                    append("Sign In")
+                }
+            }, onClick = { /*TODO*/ })
+        }
     }
 }
--- app/src/main/java/pw/alk/spendle/ui/theme/Color.kt
+++ app/src/main/java/pw/alk/spendle/ui/theme/Color.kt
@@ -2,7 +2,8 @@ package pw.alk.spendle.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Black200 = Color(0xFF203239)
+val Black200 = Color(0xFF20262E)
 val Black500 = Color(0xFF1B1A17)
 val Black700= Color(0xFF100F0F)
 val Teal200 = Color(0xFFAEFEFF)
+val White80 = Color(0xCCFFFFFF)
